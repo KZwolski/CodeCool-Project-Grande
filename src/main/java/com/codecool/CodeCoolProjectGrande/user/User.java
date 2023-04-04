a02d388ab99258dac70179c43264a6c98204d9b0
Matt <mattwasilewski96@gmail.com>
2023-04-04 22:57:45 +0200

    removed bantoken from userentity

--- src/main/java/com/codecool/CodeCoolProjectGrande/user/User.java
+++ src/main/java/com/codecool/CodeCoolProjectGrande/user/User.java
@@ -36,9 +36,6 @@ public class User {
     @JoinColumn(name = "tokenId")
     @OneToOne(cascade=CascadeType.ALL)
     private ResetPasswordToken resetPasswordToken;
-    @JoinColumn(name = "banId")
-    @OneToOne(cascade=CascadeType.ALL)
-    private BanToken banToken;
 
     public User(@NotNull String name, @NotNull String password, @NotNull String email) {
         this.name = name;
